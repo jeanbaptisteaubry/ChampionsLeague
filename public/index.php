8ef607a822e49ebda7f22cdfe0136504df6a1558
jeanbaptisteaubry <jean-baptiste.aubry@ac-besancon.fr>
2025-10-19 20:44:33 +0200

    gdnwdgn

--- public/index.php
+++ public/index.php
@@ -131,7 +131,7 @@ $app->add(function (Request $request, RequestHandler $handler) use ($responseFac
 });
 
 // Admin area
-$app->group('/admin', function ($group) use ($admin, $aParierModel) {
+$app->group('/admin', function ($group) use ($admin, $aParierModel, $adminReminder, $phaseModel) {
     $group->get('', [$admin, 'home']);
     $group->get('/home-text', [$admin, 'showHomeText']);
     $group->post('/home-text', [$admin, 'saveHomeText']);
